3173f749bce12cb3689ffc16fb46a73538995981
tester01 <sdd03@naver.com>
2024-03-05 17:26:16 +0900

    202403051730

--- src/components/layouts/mains/title/details/LayerPopDicPerson.vue
+++ src/components/layouts/mains/title/details/LayerPopDicPerson.vue
@@ -1,34 +1,40 @@
 <template>
 <div class="pop_dictionary">
-  <div class="pop_layer">
-    <div class="flag_section">
-      <button type="button" class="btn_prev" @click="fnSetMainFlag('P')"></button>
-      <h1>{{ currFlag.flagName }}</h1>
-      <button type="button" class="btn_next" @click="fnSetMainFlag('N')"></button>
-    </div>
-    <div class="scenario_section">
-      <div :class="'flag_info_section'+(scenarioObj && scenarioObj.idx === scenario.idx ? ' on' : '')" v-for="scenario in scenarioArr" :key="scenario.idx" :aria-disabled="scenario.useYn ? 'false' : 'true'" @click="fnCallScenarioDetail(scenario)">
-        <h3 class="flag_info_item">UC {{ scenario.date }}</h3>
-        <h3 class="flag_info_item">{{ scenario.subtitle }}</h3>
+  <div class="pop_dictionary_layer">
+    <div class="scenario_list_section">
+      <div :class="'scenario_row_section'+(scenarioObj.rn === scenarioRow.rn ? ' on' : '')" v-for="scenarioRow in scenarioArr" :key="scenarioRow.rn" @click="fnSetScenario(scenarioRow.rn)">
+          <h4 class="flag_info_item">UC {{ scenarioRow.date }}</h4>
+          <h3 class="flag_info_item">{{fnGetScenarioFlagName(scenarioRow.flag)}} {{ (scenarioRow.subtitle || '시나리오 이름 작성중') }}</h3>
       </div>
     </div>
+    <div class="scenario_section">
+      <button type="button" class="btn_prev" @click="fnBtnScenario('P')"></button>
+        <div @click="fnToggleSceneList">
+          <h4 class="flag_info_item">UC {{ scenarioObj.date }}</h4>
+          <h3 class="flag_info_item">{{fnGetScenarioFlagName(scenarioObj.flag)}} {{ (scenarioObj.subtitle || '시나리오 이름 작성중') }} </h3>
+        </div>
+      <button type="button" class="btn_next" @click="fnBtnScenario('N')"></button>
+    </div>
+    <div class="character_search_section">
+      <input type="text" class="">
+    </div>
+
+    <div class="character_list_section">
+      <div v-for="characterRow in characterArr" :key="characterRow.RN">
+        <h3>{{characterRow.CHA_STD_NAME}}</h3>
+      </div>
+      
+    </div>
     
-    <div class="character_section">
-      aa
-    </div>
-    <div class="scenario_detail_section on" v-if="scenarioPop">
-      <scenario-detail-info :scenarioObj="scenarioObj" @fnClosePop="fnCloseScenarioDetailInfo"/>
-    </div>
   </div>
 </div>
 </template>
 
 <script>
 import { scenarioMeta } from "@/assets/txt/scenario/scenarioMeta";
-import scenarioDetailInfo from '@/components/layer/item/scenarioDetailInfoArea.vue'
+import * as XLSX from 'xlsx';
 export default {
   components: {
-    scenarioDetailInfo,
   },
   data() {
     return {
@@ -38,54 +44,99 @@ export default {
         {idx: 1, flagName:'스탠다드', flagCode:'S'},
         {idx: 2, flagName:'가상역사', flagCode:'F'}
       ],
-      //선택된 TYPE
-      currFlag : {
-        idx: 0,
-        flagName:'',
-        flagCode:''
-      },
-      //새 게임의 시나리오
       scenarioArr : [],
-      //선택된 시나리오
       scenarioObj : {},
-      //시니리오 팝업 컨트롤러
-      scenarioPop : false,
+
+      characterArr: [],
     };
+  },
+  watch: {
+    'scenarioObj.rn'() {
+      this.fnGetCharacterInfo();
+    },
+
   },
   mounted() {
     this.userData = this.$store.getters['storeUser/getCurrentUser'];
-    this.fnSetMainFlag();
+    console.info('userData', this.userData);
+    this.fnInitScenarioMeta();
   },
   methods : {
-    fnSetMainFlag(type) {
-      if (this.currFlag.idx === undefined) {
-        this.currFlag.idx = 0;
+    //시나리오
+    fnInitScenarioMeta() {
+      this.scenarioArr = scenarioMeta;
+      this.scenarioArr.sort((a, b) => {
+        const dateA = new Date(a.date);
+        const dateB = new Date(b.date);
+        return dateA - dateB;
+      });
+      this.scenarioArr.forEach((row, index) => {
+        row.rn = index;
+      });
+      if(this.scenarioArr && this.scenarioArr.length > 0) {
+        this.scenarioObj = this.scenarioArr[0];
       }
-      let newIdx = this.currFlag.idx;
-      if (type === 'N') {
-        newIdx = (this.currFlag.idx + 1) % this.menuFlag.length;
-      } else if (type === 'P') {
-        newIdx = (this.currFlag.idx - 1 + this.menuFlag.length) % this.menuFlag.length;
-      }
-      this.currFlag = this.menuFlag[newIdx];
-      this.fnCallScenario(this.currFlag.flagCode);
     },
-    fnCallScenario(flg) {
-      this.scenarioArr = scenarioMeta.filter(scenario => scenario.flag === flg);
-      this.scenarioObj = {};
+    fnGetScenarioFlagName(flagCode) {
+      const matchingFlag = this.menuFlag.find(flag => flag.flagCode === flagCode);
+      return `[${matchingFlag?.flagName || ''}]`;
     },
-    fnCallScenarioDetail(data) {
-      if(!data.useYn) {
-        return;
-      }
-      this.scenarioObj = data;
-      this.fnSetScenarioDetailInfo();
-    },
-    fnSetScenarioDetailInfo() {
-      // this.scenarioPop = true;
-    },
-    
+    fnBtnScenario(flg) {
+      let index = this.scenarioObj.rn;
+      const arrLength = this.scenarioArr.length;
 
+      if(flg === 'P') {
+        index = (index - 1 + arrLength) % arrLength;
+      } else if(flg === 'N') {
+        index = (index + 1) % arrLength;
+      }
+      this.scenarioObj = this.scenarioArr[index];
+    },
+    fnToggleSceneList() {
+      // 현재 클래스 상태 확인
+      const isOn = this.$el.querySelector('.scenario_list_section').classList.contains('on');
+      // 클래스 토글
+      this.$el.querySelector('.scenario_list_section').classList.toggle('on', !isOn);
+    },
+    fnSetScenario (rn) {
+      this.scenarioObj = this.scenarioArr[rn];
+      this.fnToggleSceneList();
+    },
+    //캐릭터
+    async fnGetCharacterInfo() {
+      await this.fnGetScenarioCharData();
+    },
+    // 엑셀에서 데이터를 가져온다.
+    async fnGetScenarioCharData() {
+      let filePath = 'data/scenario/TN_GEN_CHAR.xlsx';
+      if (this.scenarioObj.mod === 'Y' && this.scenarioObj.id) {
+        filePath = `data/scenario/${this.scenarioObj.id}/TN_GEN_CHAR.xlsx`;
+      }
+
+      const reader = new FileReader();
+      reader.onload = () => {
+        const arrayBuffer = reader.result;
+        const workbook = XLSX.read(new Uint8Array(arrayBuffer), { type: 'array' });
+        const sheetName = workbook.SheetNames[0];
+        const sheet = workbook.Sheets[sheetName];
+        const jsonData = XLSX.utils.sheet_to_json(sheet);
+        if( jsonData && jsonData.length > 0) {
+          this.$store.commit('storeScene/setCharacterList', jsonData);
+          this.characterArr = jsonData;
+          console.info('characterArr', this.characterArr)
+        } else {
+          console.error('캐릭터 정보 NULL');
+        }
+      };
+      try {
+        fetch(filePath)
+        .then(response => response.blob())
+        .then(blob => reader.readAsArrayBuffer(blob));  
+      } catch (error) {
+        console.error(error)
+      }
+      
+    },
   }
 }
 </script>
@@ -93,20 +144,26 @@ export default {
 <style lang="scss" scoped>
   @charset "UTF-8";
 .pop_dictionary {
-  min-height: 100%;
-  .pop_layer {
-    z-index: 500 !important;
-    min-height: 100%;
+  height: auto;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  .pop_dictionary_layer {
+    position: relative;
+    z-index: 500;
+    height: 100%;
     display: flex;
     flex-direction: column;
-
-    .flag_section {
-      background-color: #c2daf7;
-      color: black;
+    .scenario_section {
+      position: relative;
+      top: 0;
       display: flex;
       justify-content: space-between;
       align-items: center;
-      flex: 1;
+      overflow: auto;
+      height: 3rem;
+      border-top: 3px solid #ffffff;
+      border-bottom: 3px solid #ffffff;
       .btn_prev {
         background-image: url("@/assets/images/common/icon/play_arrow.png");
         width: 2rem;
@@ -122,60 +179,42 @@ export default {
         background-size: cover;
         border: none;
       }
-      h1 {
-        flex: 1;
-        text-align: center;
-        white-space: nowrap;
-        overflow: hidden;
-        position: relative;
+    }
+    .scenario_list_section {
+      top: 3rem;
+      position: absolute;
+      overflow: hidden;
+      transition: height 3s ease;
+      height: 0;
+      max-height: 40rem;
+      width: 100%;
+      background-color: black;
+      z-index: 500;
+      border: none;
+      overflow-y: auto;
+      .scenario_row_section {
+        margin: 0.5rem;
+        border-bottom: 1px solid #ffffff;
       }
-      h1::before {
-        content: "";
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        background: linear-gradient(to right, transparent, rgb(111, 62, 156), transparent);
-        animation: textFlow 5s linear infinite;
-      }
-      @keyframes textFlow {
-        0% {
-          transform: translateX(-100%);
-        }
-        100% {
-          transform: translateX(100%);
-        }
+      .scenario_row_section.on {
+        border: 2px solid white;
+        
       }
     }
-    .scenario_section {
-      max-height: 25vh;
-      overflow: auto;
-      flex: 1;
-      .flag_info_section {
-        margin : 0.8rem;
-        color : #c2daf7;
-        max-height: 8rem;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        align-items: center;
-        .flag_info_item {
-          flex: 1;
-        }
-        &.on {
-          box-shadow: 0 0 0 2px #c2daf7;
-          border-radius: 0.5em;
-        }
-      }
-      .flag_info_section[aria-disabled="true"] {
-        color:gray;
+    .scenario_list_section.on {
+      height: auto;
+    }
+    .character_search_section {
+      border: 2px solid white;
+      input {
+        width: 80%;
+        height: 2rem;
       }
     }
-    .character_section {
-      flex-grow: 2;
-      overflow: auto;
-      background-color: red;
+    .character_list_section {
+      position: relative;
+      max-height: 200px;
+      overflow-y: auto;
     }
 
     // 하단 버튼 에리어
@@ -215,6 +254,4 @@ export default {
     }
   }
 }
-
-
 </style>
